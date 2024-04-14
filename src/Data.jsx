d48f77f5981f48c223c1699199abc87ed31fec14
Hamza3357 <hamzaawan3357@gmail.com>
2024-04-14 11:38:30 +0500

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "hamza",
+        email: "hamzasabir3357@gmail.com",
+        id: 1
+    },
+    {
+        name: "fahad",
+        email: "fahadmir742@gmail.com",
+        id: 2
+    },
+    {
+        name: "ahmed",
+        email: "ahmed572@gmail.com",
+        id: 3
+    }
+];
